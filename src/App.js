5649a039dca0fe2e17e640460543b0638f1321db
Hamza-Mujahid <80825408+Hamza-Mujahid@users.noreply.github.com>
2021-11-28 23:27:44 +0500

    tea,-reporter

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './Pages/Home Page/Home'
+
+
+let App = () => {
+  return (
+    <div className="App">
+     <Counter />
+     </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home Page/Home.js
@@ -0,0 +1,11 @@
+import './Home.css';
+
+let Counter = () => {
+    return (
+        <div>
+           Welcome to the home page
+        </div>
+    );
+}
+
+export default Counter;
